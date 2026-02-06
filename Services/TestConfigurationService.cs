4b1f4672e4a2a8af21aa431a13ea2bef79f9f7bb
Hemanath <pm4130424@gmail.com>
2026-02-06 09:27:08 +0530

    API Response In TestConfigration

--- Services/TestConfigurationService.cs
+++ Services/TestConfigurationService.cs
@@ -251,7 +251,421 @@ namespace NTTApiTesting.Services
 
             });
 
+            testCases.Add(new ApiTestCase
+            {
+                Name = "BasketOrder",
+                Method = "POST",
+                Url = "{{endpoint}}BasketOrder",
+                Headers = new Dictionary<string, string>
+                    {
+                        { "AuthToken", "{{token}}" },
+                        { "Module", "OrderService" },
+                        { "Source", "RMS" }
+                    },
+                                Body = @"
+{
+                          ""AllOrders"": [
+                            {
+                              ""uid"": ""89191333"",
+                              ""actid"": ""89191333"",
+                              ""exch"": ""MCX"",
+                              ""trantype"": ""Sell"",
+                              ""norenordno"": ""0"",
+                              ""segment"": ""CUR"",
+                              ""tsym"": ""CRUDEOIL14JAN20266550CE"",
+                              ""qty"": 1,
+                              ""prc"": ""0.00"",
+                              ""trgprc"": ""0"",
+                              ""dscqty"": 0,
+                              ""prd"": ""CNC"",
+                              ""prctyp"": ""MKT"",
+                              ""mkt_protection"": """",
+                              ""ret"": ""DAY"",
+                              ""remarks"": """",
+                              ""ordersource"": ""Web"",
+                              ""bpprc"": ""0"",
+                              ""blprc"": ""0"",
+                              ""trailprc"": ""0"",
+                              ""ext_remarks"": ""External remarks"",
+                              ""cl_ord_id"": """",
+                              ""tsym2"": """",
+                              ""trantype2"": """",
+                              ""qty2"": ""0"",
+                              ""prc2"": ""0"",
+                              ""tsym3"": ""0"",
+                              ""trantype3"": """",
+                              ""qty3"": ""0"",
+                              ""prc3"": ""0"",
+                              ""algo_id"": ""0"",
+                              ""naic_code"": ""0"",
+                              ""snonum"": ""0"",
+                              ""filshares"": ""0"",
+                              ""rorgqty"": ""0"",
+                              ""orgtrgprc"": ""0""
+                            },
+                            {
+                              ""uid"": ""89191333"",
+                              ""actid"": ""89191333"",
+                              ""exch"": ""MCX"",
+                              ""trantype"": ""Buy"",
+                              ""norenordno"": ""0"",
+                              ""segment"": ""CUR"",
+                              ""tsym"": ""CRUDEOIL14JAN20266600CE"",
+                              ""qty"": 1,
+                              ""prc"": ""0.00"",
+                              ""trgprc"": ""0"",
+                              ""dscqty"": 0,
+                              ""prd"": ""CNC"",
+                              ""prctyp"": ""MKT"",
+                              ""mkt_protection"": """",
+                              ""ret"": ""DAY"",
+                              ""remarks"": """",
+                              ""ordersource"": ""Web"",
+                              ""bpprc"": ""0"",
+                              ""blprc"": ""0"",
+                              ""trailprc"": ""0"",
+                              ""ext_remarks"": ""External remarks"",
+                              ""cl_ord_id"": """",
+                              ""tsym2"": """",
+                              ""trantype2"": """",
+                              ""qty2"": ""0"",
+                              ""prc2"": ""0"",
+                              ""tsym3"": ""0"",
+                              ""trantype3"": """",
+                              ""qty3"": ""0"",
+                              ""prc3"": ""0"",
+                              ""algo_id"": ""0"",
+                              ""naic_code"": ""0"",
+                              ""snonum"": ""0"",
+                              ""filshares"": ""0"",
+                              ""rorgqty"": ""0"",
+                              ""orgtrgprc"": ""0""
+                            }
+                  ]
+                }",
+                                ExpectedStatusCode = 200
+                            });
 
+            testCases.Add(new ApiTestCase
+            {
+                Name = "ModifyOrder",
+                Method = "POST",
+                Url = "{{endpoint}}ModifyOrder",
+                Headers = new Dictionary<string, string>
+                {
+                    { "AuthToken", "{{token}}" },
+                    { "Module", "OrderService" },
+                    { "Source", "RMS" }
+                },
+                Body = @"{
+                    ""exch"": ""NSE"",
+                    ""ordno"": ""1100000000121694"",
+                    ""prctyp"": ""LMT"",
+                    ""prc"": ""158.9"",
+                    ""qty"": 2,
+                    ""tqty"":10,
+                    ""tsym"": ""IOC-EQ"",
+                    ""ret"": """",
+                    ""mkt_protection"": ""0"",
+                    ""trgprc"": ""0"",
+                    ""dscqty"": 0,
+                    ""ext_remarks"": """",
+                    ""cl_ord_id"": ""170256153"",
+                    ""uid"": ""47054457"",
+                    ""actid"": ""47054457"",
+                    ""bpprc"": ""0"",
+                    ""blprc"": ""0"",
+                    ""trailprc"": ""0"",
+                    ""ordersource"": ""Web"",
+                    ""orderactivity"": 2,
+                    ""prd"": ""CNC""
+                }",
+                ExpectedStatusCode = 200
+
+            });
+
+            testCases.Add(new ApiTestCase
+            {
+                Name = "MWList",
+                Method = "POST",
+                Url = "{{endpoint}}MWList",
+                Headers = new Dictionary<string, string>
+                {
+                    { "AuthToken", "{{token}}" },
+                    { "Module", "OrderService" },
+                    { "Source", "RMS" }
+                },
+                Body = @"{
+                    
+                }",
+                ExpectedStatusCode = 200
+
+            });
+
+            testCases.Add(new ApiTestCase
+            {
+                Name = "CancelOrder",
+                Method = "POST",
+                Url = "{{endpoint}}CancelOrder",
+                Headers = new Dictionary<string, string>
+                {
+                    { "AuthToken", "{{token}}" },
+                    { "Module", "OrderService" },
+                    { "Source", "RMS" }
+                },
+                Body = @"{
+                ""actid"": ""47054457"",
+                ""uid"": ""47054457@123"",
+                ""ordno"": ""150255663"",
+                ""cl_ord_id"": ""150255663""
+                    
+                }",
+                ExpectedStatusCode = 200
+
+            });
+            testCases.Add(new ApiTestCase
+            {
+                Name = "OrderBook",
+                Method = "POST",
+                Url = "{{endpoint}}OrderBook",
+                Headers = new Dictionary<string, string>
+                {
+                    { "AuthToken", "{{token}}" },
+                    { "Module", "OrderService" },
+                    { "Source", "RMS" }
+                },
+                Body = @"{
+                ""actid"":""47054457"",
+                ""uid"":""47054457""
+                    
+                }",
+                ExpectedStatusCode = 200
+
+            });
+            testCases.Add(new ApiTestCase
+            {
+                Name = "PositionBook",
+                Method = "POST",
+                Url = "{{endpoint}}PositionBook",
+                Headers = new Dictionary<string, string>
+                {
+                    { "AuthToken", "{{token}}" },
+                    { "Module", "OrderService" },
+                    { "Source", "RMS" }
+                },
+                Body = @"{
+                ""uid"":""47054457"",
+                ""actid"":""47054457"",
+                ""ClientType"":""""
+                    
+                }",
+                ExpectedStatusCode = 200
+
+            });
+            testCases.Add(new ApiTestCase
+            {
+                Name = "GetUsedMargin",
+                Method = "POST",
+                Url = "{{endpoint}}GetUsedMargin",
+                Headers = new Dictionary<string, string>
+                {
+                    { "AuthToken", "{{token}}" },
+                    { "Module", "OrderService" },
+                    { "Source", "RMS" }
+                },
+                Body = @"{
+                 ""uid"": ""47054457"",
+                ""actid"":  ""47054457""
+                    
+                }",
+                ExpectedStatusCode = 200
+
+            });
+            testCases.Add(new ApiTestCase
+            {
+                Name = "Limits",
+                Method = "POST",
+                Url = "{{endpoint}}Limits",
+                Headers = new Dictionary<string, string>
+                {
+                    { "AuthToken", "{{token}}" },
+                    { "Module", "OrderService" },
+                    { "Source", "RMS" }
+                },
+                Body = @"{
+                 ""uid"": ""47054457"",
+                 ""actid"": ""47054457"",
+                 ""ClientType"": """",
+                 ""SearchText"":""""
+                    
+                }",
+                ExpectedStatusCode = 200
+
+            });
+            testCases.Add(new ApiTestCase
+            {
+                Name = "TradeBook",
+                Method = "POST",
+                Url = "{{endpoint}}TradeBook",
+                Headers = new Dictionary<string, string>
+                {
+                    { "AuthToken", "{{token}}" },
+                    { "Module", "OrderService" },
+                    { "Source", "RMS" }
+                },
+                Body = @"{
+                  ""uid"":""47054457"",
+                    ""ClientType"":"""",
+                 ""actid"":""47054457""
+                    
+                }",
+                ExpectedStatusCode = 200
+
+            });
+            testCases.Add(new ApiTestCase
+            {
+                Name = "GetUserInfo",
+                Method = "POST",
+                Url = "{{endpoint}}GetUserInfo",
+                Headers = new Dictionary<string, string>
+                {
+                    { "AuthToken", "{{token}}" },
+                    { "Module", "OrderService" },
+                    { "Source", "RMS" }
+                },
+                Body = @"{
+                   ""actid"": ""47054457""                    
+                }",
+                ExpectedStatusCode = 200
+
+            });
+            testCases.Add(new ApiTestCase
+            {
+                Name = "AlterTOTP",
+                Method = "POST",
+                Url = "{{endpoint}}AlterTOTP",
+                Headers = new Dictionary<string, string>
+                {
+                    { "AuthToken", "{{token}}" },
+                    { "Module", "OrderService" },
+                    { "Source", "RMS" }
+                },
+                Body = @"{
+                   ""uid"":""47054457"",
+                    ""IsTOTPDisable"":1                    
+                }",
+                ExpectedStatusCode = 200
+
+            });
+
+            testCases.Add(new ApiTestCase
+            {
+                Name = "GetLastOrderStatus",
+                Method = "POST",
+                Url = "{{endpoint}}GetLastOrderStatus",
+                Headers = new Dictionary<string, string>
+                {
+                    { "AuthToken", "{{token}}" },
+                    { "Module", "OrderService" },
+                    { "Source", "RMS" }
+                },
+                Body = @"{
+                    ""uid"":""47054457"",
+                  ""actid"":""47054457""                   
+                }",
+                ExpectedStatusCode = 200
+
+            });
+
+            testCases.Add(new ApiTestCase
+            {
+                Name = "GetLastAMOOrderStatus",
+                Method = "POST",
+                Url = "{{endpoint}}GetLastAMOOrderStatus",
+                Headers = new Dictionary<string, string>
+                {
+                    { "AuthToken", "{{token}}" },
+                    { "Module", "OrderService" },
+                    { "Source", "RMS" }
+                },
+                Body = @"{
+                  ""actid"": ""47054457"",
+                 ""uid"": ""47054457""
+            }",
+                ExpectedStatusCode = 200
+
+            });
+
+            testCases.Add(new ApiTestCase
+            {
+                Name = "ProductConversion",
+                Method = "POST",
+                Url = "{{endpoint}}ProductConversion",
+                Headers = new Dictionary<string, string>
+                {
+                    { "AuthToken", "{{token}}" },
+                    { "Module", "OrderService" },
+                    { "Source", "RMS" }
+                },
+                Body = @"{
+                  ""uid"": ""75885422"",
+                    ""login_token"": ""UQUHixrgPBGHp8L6aYENVLNTZZ0vTiRjEpK7ecBIk1u0JP9FIyPRBaHLHD597MB6O5jcB4xxZmbD67tZ+BXcH8bjZK6xzLgHZ1dru8lfs7U="",
+                    ""actid"": ""75885422"",
+                    ""UserId"": ""75885422"",
+                    ""usrID"": ""75885422"",
+                    ""end_type"": ""ProductConversion"",
+                    ""exch"": ""NFO"",
+                    ""trantype"": ""Buy"",
+                    ""segment"": ""Derivative"",
+                    ""series"": ""EQ"",
+                    ""tsym"": ""NIFTY2620325500CE"",
+                    ""qty"": ""72"",
+                    ""prc"": ""0"",
+                    ""prd"": ""CNC"",
+                    ""remarks"": ""Product conversion"",
+                    ""prevprd"": ""MIS"",
+                    ""postype"": ""Day"",
+                    ""token"": ""49808"",
+                    ""ordersource"": ""ios"",
+                    ""ext_remarks"": ""External remarks"",
+                    ""deviceid"": ""C273143B-4D07-4E20-9A3B-70AB7A2C61DA"",
+                    ""device_type"": ""2"",
+                    ""IP_ADDRESS"": ""106.210.165.8"",
+                    ""device"": ""ios"",
+                    ""BOW_API_VERSION"": ""1.0.4"",
+                    ""pna"": ""com.navia.all"",
+                    ""App_Version"": ""5.5"",
+                    ""app_type"": """",
+                    ""norenordno"": ""0"",
+                    ""trgprc"": ""0"",
+                    ""dscqty"": ""0"",
+                    ""prctyp"": ""Limit"",
+                    ""mkt_protection"": ""5%"",
+                    ""ret"": ""Type X"",
+                    ""bpprc"": ""0"",
+                    ""blprc"": ""0"",
+                    ""trailprc"": ""0"",
+                    ""cl_ord_id"": ""123"",
+                    ""amo"": ""Yes"",
+                    ""tsym2"": """",
+                    ""trantype2"": """",
+                    ""qty2"": ""0"",
+                    ""prc2"": ""0"",
+                    ""tsym3"": ""0"",
+                    ""trantype3"": """",
+                    ""qty3"": ""0"",
+                    ""prc3"": ""0"",
+                    ""algo_id"": ""0"",
+                    ""naic_code"": ""0"",
+                    ""snonum"": ""0"",
+                    ""filshares"": ""0"",
+                    ""fillshares2"": ""-11111111"",
+                    ""rorgqty"": ""0"",
+                    ""orgtrgprc"": ""0""
+            }",
+                ExpectedStatusCode = 200
+
+            });
 
 
 
